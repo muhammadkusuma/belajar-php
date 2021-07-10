eb5b79ccb613db51d77161e25adc4ecdacc71a7f
muhammadkusuma <mwiraadekusuma1@gmail.com>
2021-07-10 11:40:24 +0700

    Operator pada PHP

---
+++ operator.php
@@ -0,0 +1,47 @@
+<?php
+
+// Operator Aritmatika
+// +, -, /, *
+
+/*
+$a=10;
+$b=20;
+
+$total= $a+$b
+
+echo $total;
+*/
+
+// Operator Assignment
+
+    // $a=30;
+    // $b=50;
+
+    // $total = 10;
+
+    // // $total = $total + $a;
+    // // $total += $a;
+
+    // echo $total;
+
+
+// Operator Pembanding
+    // $a = 7;
+    // $b = 4;
+
+    // $komparasi = $a !== $b;
+
+    // echo var_dump($komparasi);
+
+
+// OPerator <Increement/decdreement
+    // $a=10;
+    // $a++;
+    // // echo $a
+
+// Operator String
+
+$a="Hellow";
+$b=" Gaees";
+$akhir=$a.$b;
+echo $akhir;
