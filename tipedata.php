d8de515478e45c5e54c66d39f5ccc3e2635b46e2
muhammadkusuma <mwiraadekusuma1@gmail.com>
2021-07-10 11:25:58 +0700

    Tipe Data pada PHP

---
+++ tipedata.php
@@ -0,0 +1,25 @@
+<?php
+
+// Tipe Data String
+$a = "Ini adalah string";
+
+// Tipe Data Integer
+$b = 10;
+
+// Tipe Data Float
+$c = 12.5;
+
+// Tipe Data Boelan
+$d = false;
+
+// Tipe Data Array
+$e = [
+    'satu',
+    'dua',
+    'tiga'
+];
+
+// Tipe Data Null
+$f = Null;
+
+echo var_dump($a);
