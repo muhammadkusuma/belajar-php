2237d3588cdd8031a4c4fd4152f4e8f0f7a8834e
muhammadkusuma <mwiraadekusuma1@gmail.com>
2021-07-10 11:19:39 +0700

    Variable Pada PHP

---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+
+$teks = "Halo dunia";
+$angka = 110;
+$angkadua = 11.55;
+
+echo $teks;
+echo $angka;
+echo $angkadua;
+
+
+
+// Aturan
+/*
+Besar kecil harus diperhatikan
+Angka, Tanda baca, strips diawal
+
+*/
