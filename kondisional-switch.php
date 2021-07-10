edb566fe9351b85508333edd800397711c0889e9
muhammadkusuma <mwiraadekusuma1@gmail.com>
2021-07-10 12:32:55 +0700

    Switch

---
+++ kondisional-switch.php
@@ -0,0 +1,21 @@
+<?php
+
+$warna="merah";
+
+switch ($warna) {
+    case 'merah':
+        echo "berhenti";
+        break;
+    
+    case 'kuning':
+        echo "hati-hati";
+        break;
+    
+    case 'hijau':
+        echo "maju";
+        break;
+    
+    default:
+        echo "lampu rusak gaes";
+        break;
+}
